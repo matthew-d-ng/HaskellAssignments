c679e59258be203f1408d2db6167e08852a1ef88
Matthew Ng <ngm1@tcd.ie>
2018-10-10 20:07:22 +0100

    files from prev Ex00

---
+++ Exercise01/.stack-work/dist/x86_64-linux/Cabal-1.22.5.0/build/autogen/Paths_ex01.hs
@@ -0,0 +1,35 @@
+module Paths_ex01 (
+    version,
+    getBinDir, getLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+bindir, libdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/home/matthew/code/CS3016-1819/Exercise01/.stack-work/install/x86_64-linux/lts-6.19/7.10.3/bin"
+libdir     = "/home/matthew/code/CS3016-1819/Exercise01/.stack-work/install/x86_64-linux/lts-6.19/7.10.3/lib/x86_64-linux-ghc-7.10.3/ex01-0.1.0.0-CmkBIqRQqyCJ0gZevCCGZy"
+datadir    = "/home/matthew/code/CS3016-1819/Exercise01/.stack-work/install/x86_64-linux/lts-6.19/7.10.3/share/x86_64-linux-ghc-7.10.3/ex01-0.1.0.0"
+libexecdir = "/home/matthew/code/CS3016-1819/Exercise01/.stack-work/install/x86_64-linux/lts-6.19/7.10.3/libexec"
+sysconfdir = "/home/matthew/code/CS3016-1819/Exercise01/.stack-work/install/x86_64-linux/lts-6.19/7.10.3/etc"
+
+getBinDir, getLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "ex01_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "ex01_libdir") (\_ -> return libdir)
+getDataDir = catchIO (getEnv "ex01_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "ex01_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "ex01_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
